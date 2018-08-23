dc448742266070146ff57a8b2caf8a77db02d3da
Ryan Van Rooyen <ryan@Ryans-MacBook-Pro.local>
2018-08-23 10:14:10 -0600

    Add job count to PS1.

--- theme.zsh
+++ theme.zsh
@@ -12,52 +12,35 @@ BLUE="%{$FG[038]%}"
 RESET="%{$reset_color%}"
 #PROMPT_CHAR=">>"
 
-function setGitColor {
-    git branch >/dev/null 2>/dev/null && echo "%{$fg[white]%}" && return
-    echo "%{$fg_bold[black]%}"
-}
-
-function git_prompt_info() {
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo "$(setGitColor)$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_status)$ZSH_THEME_GIT_PROMPT_SUFFIX"
-}
-
-function collapse_pwd {
-    echo $(pwd | sed -e "s,^$HOME,~,")
-}
-
 function virtualenv_info {
     [ $VIRTUAL_ENV ] && echo ''`basename $VIRTUAL_ENV`': '
 }
 
-PS1=$'\n$GRAY$(virtualenv_info)$DARK_GREEN$(collapse_pwd)%{$reset_color%} $(git_prompt_info)
-$WHITE$PROMPT_CHAR%{$reset_color%} '
+PS1=$'\n$GRAY$(virtualenv_info)$DARK_GREEN%~ $(git_prompt_info) $GRAY%(1j.[%j].)
+$WHITE$PROMPT_CHAR '
 
 # Git theming
-ZSH_THEME_GIT_PROMPT_PREFIX="$WHITE("
-ZSH_THEME_GIT_PROMPT_SUFFIX="$WHITE) %{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_MODIFIED="${BLUE}*$RESET"
+function git_prompt_info() {
+    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+    Changes=$(git status --porcelain 2> /dev/null)
+    BranchInfo=$(git status --porcelain -b 2> /dev/null)
+    Status=""
 
-git_prompt_status() {
-    INDEX=$(git status --porcelain 2> /dev/null)
-    STATUS=""
+    Ahead=$(echo $BranchInfo | grep -E 'ahead' | sed 's/^.*ahead \([0-9][0-9]*\).*$/\1/')
+    Behind=$(echo $BranchInfo | grep -E 'behind' | sed 's/^.*behind \([0-9][0-9]*\).*$/\1/')
 
-    Ahead="$(git status --porcelain -b | grep -E 'ahead' | sed 's/^.*ahead \([0-9][0-9]*\).*$/\1/')"
-    Behind="$(git status --porcelain -b | grep -E 'behind' | sed 's/^.*behind \([0-9][0-9]*\).*$/\1/')"
     # Show the branch's ahead/behind status
     if [ ! -z "$Ahead" ]; then
-	STATUS=" $Ahead${GREEN}↑$RESET$STATUS"
+	Status=" $Ahead$GREEN↑$RESET$Status"
     fi
     if [ ! -z "$Behind" ]; then
-	STATUS=" $Behind${RED}↓$RESET$STATUS"
+	Status=" $Behind$RED↓$RESET$Status"
     fi
 
-    if [[ -z $(git status --porcelain) ]]
-    then
-	STATUS="$STATUS"
-    else
-	STATUS="$ZSH_THEME_GIT_PROMPT_MODIFIED$STATUS"
+    if [ ! -z $Changes ]; then
+	Status="$BLUE*$RESET$Status"
     fi
 
-    echo $STATUS
+    echo "$WHITE(${ref#refs/heads/}$Status$WHITE)"
 }
+
